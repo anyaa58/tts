f46dde23cd1a2a44e7f2dfd405cd66ad147d6e42
aniket <aniket@gmail.com>
2026-07-06 04:57:17 +0000

    5th commit

--- web.sh
+++ web.sh
@@ -3,6 +3,6 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 echo "<h1> welcome to the website</h1>" > /var/www/html/index.html
-systemctl restart httpdi
+systemctl restart httpd
 
 
